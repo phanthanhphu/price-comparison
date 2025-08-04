92454b0740cb35b7575705b0429ef52f42c51d12
phanthanhphu <phub1706742@gmail.com>
2025-08-04 08:48:42 +0700

    Initial commit

---
+++ src/main/java/org/bsl/pricecomparison/PriceComparisonApplication.java
@@ -0,0 +1,14 @@
+package org.bsl.pricecomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceComparisonApplication.class, args);
+        System.out.println("nnm");
+    }
+
+}
